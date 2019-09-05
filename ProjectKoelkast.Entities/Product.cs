79bf46cb428e3be687ec396071fb631a0bf5d602
remko48 <remkohuisman@hotmail.nl>
2019-09-05 11:37:27 +0200

    existing project

---
+++ ProjectKoelkast.Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace ProjectKoelkast.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        
+        public virtual Category Category { get; set; }
+
+    }
+}
